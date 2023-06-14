56b57c4fd311b7a2baa05f5c21c8870bc71af3e3
Angela Lekosiotis <you@example.com>
2023-06-14 10:59:19 -0400

    a thing

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Home() {
         <Image src="/Angela.png" alt="" width="250" height="250" className="mx-auto mb-16 rounded-full" />
         <h1 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">Angela Lekosiotis</h1>
         <h2 className='text-6xl font-bold'>Mid Market Account Executive @ Vercel</h2>
-        <p className="font-bold text-gray-600 text-md md:text-xl my-16">Learning how to be an account executive 1 day at a time!
+        <p className="font-bold text-gray-600 text-md md:text-xl my-16">Learning how to be an account executive 1 day at a time
         </p>
       </section>
       <Contact />
