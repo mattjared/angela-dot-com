3a3289bae2171098c9bcf5c25be3f5885d956b70
Angela Lekosiotis <angelalekosiotis@Angelas-MBP.attlocal.net>
2023-06-14 09:59:56 -0400

    angela

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,13 @@ import Footer from "./Footer"
 
 export default function Home() {
   return (
-    <main className='bg-gray-100'>
+    <main className='bg-purple-100'>
       <section className="py-10 md:pt-24 md:pb-12 container max-w-screen-xl mx-auto px-4 text-center">
-        <Image src="/headshot.jpeg" alt="" width="250" height="250" className="mx-auto mb-16 rounded-full" />
-        <h1 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">{`my name`}</h1>
-        <h2 className='text-6xl font-bold'>Account Executive @ Vercel</h2>
-        <p className="font-normal text-gray-600 text-md md:text-xl my-16">Selling software and keepign it A1 since Day 1</p>
+        <Image src="/Angela.png" alt="" width="250" height="250" className="mx-auto mb-16 rounded-full" />
+        <h1 className="font-medium text-gray-600 text-lg md:text-2xl uppercase mb-8">Angela Lekosiotis</h1>
+        <h2 className='text-6xl font-bold'>Mid Market Account Executive @ Vercel</h2>
+        <p className="font-bold text-gray-600 text-md md:text-xl my-16">Learning how to be an account executive 1 day at a time
+        </p>
       </section>
       <Contact />
       <Tiles />
